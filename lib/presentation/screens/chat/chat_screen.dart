05ffe6811ccdce3b1031e756f86d09e3966a0600
Christian Ivan Cazarin de Anda <c.cazarin@outlook.com>
2024-03-27 00:22:39 -0600

    Aplicación mini chat (Yes No Maybe App) solo diseño

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(2.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://images.crunchbase.com/image/upload/c_thumb,h_170,w_170,f_auto,g_faces,z_0.7,b_white,q_auto:eco,dpr_1/qq2ofxh8q7yvfgk9iixo'),
+          ),
+        ),
+        title: const Text('Jennifer Aniston'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return(index % 2 == 0)
+                    ? const HerMessageBubble()
+                    : const MyMessageBubble();
+            })),
+
+            // Caja de texto
+            const MessageFieldBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
